5fb19b81dde66f130df0ae227b9ebd8f8e5efcd7
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2022-02-23 13:56:10 +0900

    fs/promisesモジュールが読み込めなかったので修正

--- src/getOAuth2ClientForLocal.ts
+++ src/getOAuth2ClientForLocal.ts
@@ -2,7 +2,6 @@
 import * as path from 'path';
 import { Auth, google } from 'googleapis';
 import * as fs from 'fs';
-import * as fspromises from 'fs/promises';
 
 export async function getOAuth2ClientForLocal(): Promise<Auth.OAuth2Client> {
     const options = await getOAuth2ClientOptions();
@@ -18,7 +17,7 @@ async function getOAuth2ClientOptions(): Promise<Auth.OAuth2ClientOptions> {
     if (!fs.existsSync(keyPath))
         throw new Error('not found the client secret file');
 
-    const file = await fspromises.readFile(keyPath, 'utf-8');
+    const file = await fs.promises.readFile(keyPath, 'utf-8');
     const { web } = JSON.parse(file) as ClientSecret;
     return {
         clientId: web.client_id,
