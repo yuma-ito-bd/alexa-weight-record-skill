160d4ea75f39414108951e999093f96b73b56100
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2022-02-26 13:46:07 +0900

    credentialsを保存するフォルダの指定方法を変更

--- src/app/authentication/saveCredentials.ts
+++ src/app/authentication/saveCredentials.ts
@@ -1,12 +1,9 @@
-import { Auth } from 'googleapis';
 import * as fspromises from 'fs/promises';
-import path from 'path';
+import { Auth } from 'googleapis';
+import { cwd } from 'process';
 
 export async function saveCredentails(credentials: Auth.Credentials) {
-    const filePath = path.join(
-        __dirname,
-        '../../credentials/google-api-credentials.json'
-    );
+    const filePath = `${cwd()}/credentials/google-api-credentials.json`;
     const data = JSON.stringify(credentials);
     await fspromises.writeFile(filePath, data);
     console.log(`Credentails file is saved at ${filePath}`);
